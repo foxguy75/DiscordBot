05ceb5a6b32bb8f8055f83a443be90faee904b51
Ben Camburn <foxguy75@gmail.com>
2023-01-28 13:13:42 -0500

    Init commit

---
+++ config/DnDDiscordBot.h
@@ -0,0 +1,6 @@
+// The configured options and settings for DnDDiscordBot
+// clang-format off
+#define DnDDiscordBot_VERSION_MAJOR 0
+#define DnDDiscordBot_VERSION_MINOR 0
+#define DnDDiscordBot_VERSION_PATCH 1
+// clang-format on
---
+++ src/main.cxx
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello DnDDiscordBot\n";
+  return 0;
+}
